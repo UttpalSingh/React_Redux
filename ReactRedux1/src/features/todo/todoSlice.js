885cad259beab66a61f30e332df0ff7bcc6b06c0
uttpal singh <uttpalsingh8@gmail.com>
2026-03-31 06:29:12 +0530

    Todo in redux

--- ReactRedux1/src/features/todo/todoSlice.js
+++ ReactRedux1/src/features/todo/todoSlice.js
@@ -11,12 +11,12 @@ export const todoSlice = createSlice({
         addTodo:(state,action) => {
             const todo = {
                 id:nanoid(),
-                text:action.payload,
+                text:action.payload.text,
             }
             state.todos.push(todo)
         },
         removeTodo : (state,action) => {
-            state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+            state.todos = state.todos.filter((todo)=>todo.id !== action.payload.id)
         },
         updateTodo: (state,action) => {
             state.todos = state.todos.map((todo)=>(todo.id === action.payload.id ?{...todo,text:action.payload.text} :todo))
@@ -24,3 +24,6 @@ export const todoSlice = createSlice({
     }
 })
  
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
