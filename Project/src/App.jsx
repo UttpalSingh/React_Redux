46e6648a75135175f8a8d3678d4e46e8e8ea500f
uttpal singh <uttpalsingh8@gmail.com>
2026-04-06 06:19:32 +0530

    initial commit

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import fetchPhotos from './api/mediaApi'
 
 const App = () => {
+
+
   return (
     <div className='h-screen w-full bg-gray-800 text-white'>
-      
+      <button onClick={()=>fetchPhotos('cat')}>get Photos</button>
     </div>
   )
 }
--- Project/src/api/mediaApi.js
+++ Project/src/api/mediaApi.js
@@ -3,3 +3,21 @@ import axios from 'axios'
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 
+
+export default async function fetchPhotos(query,page=1,perPage=20){
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+        params:{query,page,perPage},
+        headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
+    })
+    return response.data 
+}
+
+export default async function fetchVideos(query,page=1,perPage=15){
+        const response = await axios.get('https://api.pexels.com/v1/videos/search',{
+        params:{query,page,perPage},
+        headers:{Authorization:PEXELS_KEY}
+    })
+
+    console.log(response);
+    
+}
