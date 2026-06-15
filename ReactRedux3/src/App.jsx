ca8bd6b39f183c969b10b8d63b02de68714d7666
uttpal singh <uttpalsingh8@gmail.com>
2026-06-15 15:16:59 +0530

    initial commit

---
+++ ReactRedux3/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+    <div>
+      <h1 className=''>Hloo world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactRedux3/src/conf/config.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL)
+}
+
+
+
+
+export default conf
