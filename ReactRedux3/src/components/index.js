5295f8a2ac18d7c83d72264aa95450ab82fa2277
uttpal singh <uttpalsingh8@gmail.com>
2026-06-26 23:40:44 +0530

    initial commit

--- ReactRedux3/src/components/index.js
+++ ReactRedux3/src/components/index.js
@@ -7,6 +7,11 @@ import Button from "./Button"
 import Input from "./Input"
 import RTE from "./RTE";
 import Select from './Select'
+import Signup from "./Signup";
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
 
 
-export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,RTE,Select}
+export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,RTE,Select,Signup,Login,PostCard,PostForm,AuthLayout}
---
+++ ReactRedux3/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup as SignupComponents } from '../components'
+
+export default function Signup() {
+  return (
+    <div className='py-8'>
+      <SignupComponents/>
+    </div>
+  )
+}
